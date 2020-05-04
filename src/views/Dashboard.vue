31aeb26bcbdf42964f74f82a9a3f0697c04494b4
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2020-05-04 16:26:42 +0100

    Complete task

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="dashboard">
+		<section class="header">
+			<div class="text-center py-5">
+				<h1 class="text-white">Welcome to your Dashboard</h1>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Dashboard'
+}
+</script>
